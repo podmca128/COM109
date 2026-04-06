404c0b31d409aa6e45fde7c049dda3141a8e33af
JGrant233 <Grant-J26@ulster.ac.uk>
2026-04-06 14:01:22 +0100

    index.html opening times
    javascript gym status

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//--- Contact form validation (contact.html)
+
 $(document).ready(function() {
 
   $("#contactForm").submit(function(event) {
@@ -16,3 +18,21 @@ $(document).ready(function() {
     });
 
   });
+
+
+//---Open/Closed Status (index.html)
+
+let hour = new Date().getHours();
+let message = "";
+
+// matching open times in html file
+let day = new Date().getDay();
+
+if ((day >= 1 && day <= 5 && hour >= 8 && hour < 22) ||
+    ((day === 0 || day === 6) && hour >= 8 && hour <20)) {
+    message = "Gym is open.";
+} else {
+  message = "Gym is closed.";
+}
+
+document.getElementById("status").innerText = message;
