322e268e6ad443100cdaecb819330f75152f79ad
JGrant233 <Grant-J26@ulster.ac.uk>
2026-04-06 15:02:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 //--- Contact form validation (contact.html)
+if (typeof $ !== "undefined") {
 
-//$(document).ready(function() {
+$(document).ready(function() {
 
- // $("#contactForm").submit(function(event) {
-  // event.preventDefault();
+  $("#contactForm").submit(function(event) {
+   event.preventDefault();
 
-     // let name = $("#name").val();
-     // let email = $("#email").val();
-     // let message = $("#message").val();
+      let name = $("#name").val();
+      let email = $("#email").val();
+      let message = $("#message").val();
 
-   //   if (name === "" || email === "" || message === "") {
-     //   $("#formMessage").text("All fields must be filled in.");
-     // } else {
-     //   $("#formMessage").text("Message delivered.");
-    //  }
+      if (name === "" || email === "" || message === "") {
+        $("#formMessage").text("All fields must be filled in.");
+      } else {
+        $("#formMessage").text("Message delivered.");
+      }
 
-//    });
+    });
 
-//  });
+  });
+}
 
 
 //---Open/Closed Status (index.html)
